9a3ddb16ae284e06a97a051ad4ad116c59f55f27
AlexDjangoX <alexmonk17@gmail.com>
2023-10-04 10:39:25 +0200

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import { ClerkProvider } from '@clerk/nextjs';
+import { Inter, Space_Grotesk } from 'next/font/google';
+import type { Metadata } from 'next';
 
-export const metadata = {
-  title: 'Next.js 13 with Clerk',
+import './global.css';
+
+export const metadata: Metadata = {
+  title: 'Overflow',
+  description: 'Overflow',
+  icon: '/assets/images/site-logo.svg',
 };
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter',
+});
+
+const SpaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-spaceGrotesk',
+});
 
 export default function RootLayout({
   children,
@@ -11,9 +29,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: 'primary-gradient',
+          footerActionLink: 'primary-text-gradient hover:text-primary-500',
+        },
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${SpaceGrotesk.variable}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
